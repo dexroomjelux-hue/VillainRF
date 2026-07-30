d7b93b6640586c40509013749dc6a4c2fa23dae4
dexroomjelux-hue <dexroomjelux@gmail.com>
2026-07-30 23:25:08 +0500

    Update 0_ARIF-ROSE.js

--- models/commands/0_ARIF-ROSE.js
+++ models/commands/0_ARIF-ROSE.js
@@ -1,28 +1,42 @@
 const fs = require("fs");
+const path = require("path");
+
 module.exports.config = {
   name: "rose",
-    version: "1.1.1",
+  version: "1.1.1",
   hasPermssion: 0,
   credits: "ARIF BABU", 
   description: "Just Respond",
   commandCategory: "no prefix",
-    cooldowns: 5,  
+  cooldowns: 5,  
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  let react = event.body.toLowerCase();
-  if(react.includes("rose") ||
-     react.includes("Rose") || react.includes("ROSE") || react.includes("RosE") ||
-react.includes("rose") ||
-react.includes("🌹")) {
-    var msg = {
-        body: `🌹𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐑𝐎𝐒𝐄 𝐋𝐎🌹`,attachment: fs.createReadStream(__dirname + `/ARIF-BABU/ROSE.gif`)
-      }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🌹", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = async function({ api, event }) {
+  const { threadID, messageID, body } = event;
+  if (!body) return;
 
+  const react = body.toLowerCase();
+  
+  // Triggers ki list
+  const triggers = ["rose", "🌹"];
+  
+  // Check karta hai agar message mein koi trigger hai
+  if (triggers.some(t => react.includes(t))) {
+    const pathToGif = path.join(__dirname, "ARIF-BABU", "ROSE.gif");
+
+    // File exist karti hai to attachment bhej warna sirf text
+    if (fs.existsSync(pathToGif)) {
+      api.sendMessage({
+        body: `🌹𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐑𝐎𝐒𝐄 𝐋𝐎🌹`,
+        attachment: fs.createReadStream(pathToGif)
+      }, threadID, messageID);
+    } else {
+      api.sendMessage(`🌹𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐑𝐎𝐒𝐄 𝐋𝐎🌹`, threadID, messageID);
+    }
+
+    // Reaction
+    api.setMessageReaction("🌹", event.messageID, (err) => {}, true);
   }
+};
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
