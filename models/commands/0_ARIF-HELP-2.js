253470aa0a3fcce955d31d27d976d941a48314bc
dexroomjelux-hue <dexroomjelux@gmail.com>
2026-07-30 19:09:46 +0500

    Update 0_ARIF-HELP-2.js

--- models/commands/0_ARIF-HELP-2.js
+++ models/commands/0_ARIF-HELP-2.js
@@ -123,7 +123,7 @@ module.exports.run = async function ({ api, event, args, getText }) {
   // Footer
   msg += `PAGE [ ${page}/${maxPage} ]\n`;
   msg += `Type: ${prefix}help2 <command name>\n`;
-  msg += `BOT BY OWNER MAX 🫰`;
+  msg += `🤖 THIS BOT MADE BY OWNER MAX 🫰`;
 
   // Send message
   return api.sendMessage(msg, threadID, async (err, info) => {
