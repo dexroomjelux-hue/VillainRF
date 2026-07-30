c6219bd98324831aaf9cd58513acaf4c1d9ed7be
dexroomjelux-hue <dexroomjelux@gmail.com>
2026-07-30 22:51:40 +0500

    Update 0_ARIF-KISS-YOU.js

--- models/commands/0_ARIF-KISS-YOU.js
+++ models/commands/0_ARIF-KISS-YOU.js
@@ -1,28 +1,42 @@
 const fs = require("fs");
+const path = require("path");
+
 module.exports.config = {
   name: "Kiss",
-    version: "1.1.1",
+  version: "1.1.1",
   hasPermssion: 0,
   credits: "ARIF BABU", 
   description: "Just Respond",
   commandCategory: "no prefix",
-    cooldowns: 5, 
+  cooldowns: 5, 
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  let react = event.body.toLowerCase();
-  if(react.includes("KISS") ||
-     react.includes("kiss") || react.includes("चूमा") || react.includes("Kiss") ||
-react.includes("kiss me") ||
-react.includes("KISS YOU")) {
-    var msg = {
-        body: `𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`,attachment: fs.createReadStream(__dirname + `/ARIF-BABU/KISS-YOU.gif`)
-      }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = async function({ api, event }) {
+  const { threadID, messageID, body } = event;
+  if (!body) return;
 
+  const react = body.toLowerCase();
+  
+  // Triggers ki list (lowercase mein)
+  const triggers = ["kiss", "kiss me", "kiss you", "चूमा"];
+  
+  // Check karta hai agar message mein koi trigger hai
+  if (triggers.some(t => react.includes(t))) {
+    const pathToGif = path.join(__dirname, "ARIF-BABU", "KISS-YOU.gif");
+
+    // File exist karti hai to attachment bhej warna sirf text
+    if (fs.existsSync(pathToGif)) {
+      api.sendMessage({
+        body: `𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑶 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`,
+        attachment: fs.createReadStream(pathToGif)
+      }, threadID, messageID);
+    } else {
+      api.sendMessage(`𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑶 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`, threadID, messageID);
+    }
+
+    // Reaction
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true);
   }
+};
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
