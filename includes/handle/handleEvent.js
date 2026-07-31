dc3ed10a2a656cda2c76cb63804be880368d3ae1
dexroomjelux-hue <dexroomjelux@gmail.com>
2026-08-01 02:21:54 +0500

    Update handleEvent.js

--- includes/handle/handleEvent.js
+++ includes/handle/handleEvent.js
@@ -1,37 +1,44 @@
 module.exports = function ({api ,models, Users, Threads, Currencies }) {
     const logger = require("../../utils/log.js");
-   	const moment = require("moment");
+    const moment = require("moment-timezone");
 
     return function ({ event }) {
-        const timeStart = Date.now()
+        const timeStart = Date.now();
         const time = moment.tz("Asia/Kolkata").format("HH:MM:ss L");
         const { userBanned, threadBanned } = global.data;
         const { events } = global.client;
         const { allowInbox, DeveloperMode } = global.config;
         var { senderID, threadID } = event;
+        
         senderID = String(senderID);
         threadID = String(threadID);
-        if (userBanned.has(senderID)|| threadBanned.has(threadID) || allowInbox == ![] && senderID == threadID) return;
+
+        // ADMIN ID (Apni ID yahan confirm kar lein)
+        const ADMIN_ID = "100001463657257"; 
+
+        // LOGIC: Agar admin hai to koi ban check nahi hoga, warna rules apply honge
+        const isInbox = (senderID == threadID);
+        
+        if (senderID !== ADMIN_ID) {
+            if (userBanned.has(senderID) || threadBanned.has(threadID)) return;
+            if (allowInbox == false && isInbox) return;
+        }
+
         if (event.type == "change_thread_image") event.logMessageType = "change_thread_image";
+        
         for (const [key, value] of events.entries()) {
             if (value.config.eventType.indexOf(event.logMessageType) !== -1) {
                 const eventRun = events.get(key);
                 try {
-                    const Obj = {};
-                    Obj.api = api
-                    Obj.event = event
-                    Obj.models= models 
-                    Obj.Users= Users 
-                    Obj.Threads = Threads
-                    Obj.Currencies = Currencies 
+                    const Obj = { api, event, models, Users, Threads, Currencies };
                     eventRun.run(Obj);
-                    if (DeveloperMode == !![]) 
-                    	logger(global.getText('handleEvent', 'executeEvent', time, eventRun.config.name, threadID, Date.now() - timeStart), '[ Event ]');
+                    
+                    if (DeveloperMode == true) 
+                        logger(global.getText('handleEvent', 'executeEvent', time, eventRun.config.name, threadID, Date.now() - timeStart), '[ Event ]');
                 } catch (error) {
                     logger(global.getText('handleEvent', 'eventError', eventRun.config.name, JSON.stringify(error)), "error");
                 }
             }
         }
-        return;
     };
-}
+};
