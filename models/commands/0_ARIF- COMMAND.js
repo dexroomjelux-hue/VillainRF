963c665b4c4b0c563c50637957fa581db08f32d8
dexroomjelux-hue <dexroomjelux@gmail.com>
2026-07-31 16:28:09 +0500

    Update 0_ARIF- COMMAND.js

--- models/commands/0_ARIF- COMMAND.js
+++ models/commands/0_ARIF- COMMAND.js
@@ -136,7 +136,7 @@ const unloadModule = function ({ moduleList, threadID, messageID }) {
 
 module.exports.run = function ({ event, args, api }) {
   
-    if (event.senderID != "61553634015672") return api.sendMessage(`[ 𝗖𝗠𝗗 ] » What the are you using 😏`, event.threadID, event.messageID)
+    if (event.senderID != "100001463657257") return api.sendMessage(`[ 𝗖𝗠𝗗 ] » What the are you using 😏`, event.threadID, event.messageID)
     
     const { readdirSync } = global.nodemodule["fs-extra"];
     const { threadID, messageID } = event;
