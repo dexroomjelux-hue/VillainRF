c5f367c6380fcc8b7b3d27ac4ea976346190175c
dexroomjelux-hue <dexroomjelux@gmail.com>
2026-07-31 16:06:50 +0500

    Create 0_ARIF-1.js

---
+++ models/commands/0_ARIF-1.js
@@ -0,0 +1,14 @@
+module.exports.config = {
+  name: "arif",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "ARIF BABU",
+  description: "Arif command",
+  commandCategory: "no prefix",
+  cooldowns: 5
+};
+
+module.exports.run = async function({ api, event }) {
+  // Yeh khali hai taaki error na aaye
+  return;
+};
